665595c0e79bef2478a3d133716bdc4f40792bcf
samcbehrens <samcbehrens@gmail.com>
2016-07-06 15:48:59 -0400

    adding gitignore file and going to pull transcription of audio

--- SeperateAudio.py
+++ SeperateAudio.py
@@ -3,10 +3,11 @@ import subprocess as sp
 import sys
 import numpy
 
-def extractAudio(audioPath, newFileName):
+def extractAudio(videoPath, newFileName):
 	
-	extract = "ffmpeg -i audioPath ab 160k -ac 2 -ar 44100 -vn newFileName.wav"
-	subprocess.call(extract, shell = True)
+	extract = "ffmpeg -i " + videoPath + " " + newFileName
+	# extract = "ffmpeg -i audioPath ab 160k -ac 2 -ar 44100 -vn newFileName.wav"
+	sp.call(extract, shell = True)
 
 if __name__ == "__main__":
 	extractAudio(sys.argv[1], sys.argv[2])
