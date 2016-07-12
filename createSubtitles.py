6db5cd7da2d7889f655f6bafac6fafeee75550f9
samcbehrens <samcbehrens@gmail.com>
2016-07-12 13:28:39 -0400

    finished creating the srt file

--- createSubtitles.py
+++ createSubtitles.py
@@ -5,27 +5,28 @@ subs =[]
 def loadTemplate():
 	global subs
 	subs = pysubs2.load("template.srt");
+	del subs[0:2]
 
 def createEvent(content,start, end, ):
-	
 	ev = pysubs2.SSAEvent(start=pysubs2.make_time(s=start), end=pysubs2.make_time(s=end), text=content)
 	return ev 
 
-def addEvent(event):
+def addEvent(i, event):
 	global subs  
 	print "in add event"
 	print event
-	subs.append(event)
+	subs.insert(i, event)
 
 def createTranscript(subtitleArray, captionFinalTitle):
 	loadTemplate()
 
-	for tgroup in subtitleArray:
-		event = createEvent(tgroup[0], tgroup[1], tgroup[2])
+	for x in range(len(subtitleArray)):
+		event = createEvent(subtitleArray[x][0], subtitleArray[x][1], subtitleArray[x][2])
 		print event 
-		addEvent(event)
+		addEvent(x, event)
 	
 	saveTranscript(captionFinalTitle)
+
 
 def saveTranscript(finalName):
 	global subs 
